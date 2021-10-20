b02f90981f251a063abad7f73ef2d0d9b8a85027
Elio Struyf <elio@struyfconsulting.be>
2021-10-20 12:29:59 +0200

    Added star service

---
+++ services/StarService.ts
@@ -0,0 +1,28 @@
+
+
+export class StarService {
+  private static stars: number | undefined;
+  private static starCaller: Promise<Response> | undefined;
+
+  public static async get(): Promise<number> {
+    if (StarService.stars) {
+      return StarService.stars;
+    }
+
+    if (!StarService.starCaller) {
+      StarService.starCaller = fetch('/api/stars');
+    }
+
+    const response = await StarService.starCaller;
+
+    if (response && response.ok) {
+      const data = await response.clone().json();
+      if (data && data.stars) {
+        StarService.stars = data.stars;
+        return data.stars as number;
+      }
+    }
+
+    return 0;
+  }
+}
