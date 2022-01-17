31a367e6a7f290961bc9f8a476149644d475a5f4
Elio Struyf <elio@struyfconsulting.be>
2022-01-17 12:29:34 +0100

    Added backers api

---
+++ pages/api/backers.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fetch from 'node-fetch';
+
+const api = async (req: NextApiRequest, res: NextApiResponse) => {
+  const backers = process.env.BACKERS;
+  const { query } = req;
+
+  if (!query.backer) {
+    return res.status(400).send({
+      error: 'Missing backer parameter',
+    });
+  }  
+  
+  if (!backers) {
+    return res.status(500).json({
+      error: 'No backers configured',
+    });
+  }
+
+  const allBackers = backers.split(',');
+  const backer = query.backer as string;
+  if (!allBackers.includes(backer)) {
+    return res.status(400).json({
+      error: `Sorry, it seems you haven't supported the project yet.`,
+    });
+  }
+
+  res.status(200).send(`Thanks for your support ${backer}! 🤘`);
+}
+
+export default api;
