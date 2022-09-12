db36be7408270ae6d8ee8b33a41add18bd6ce889
Elio Struyf <elio@struyfconsulting.be>
2022-09-12 10:30:10 +0200

    Added last modified date to schema

--- scripts/generate-schema.js
+++ scripts/generate-schema.js
@@ -24,6 +24,7 @@ const fetch = require('node-fetch');
             "$schema": "http://json-schema.org/draft-07/schema",
             "$id": idUrl,
             "description": "Defines the settings for Front Matter",
+            "lastModified": new Date().toISOString(),
             "type": "object",
             ...pkgJson.contributes.configuration,
             "title": "Front Matter - Team Settings"
