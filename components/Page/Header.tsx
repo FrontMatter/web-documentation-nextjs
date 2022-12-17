6e459ffeaa433069cc41bbe9856fef26793c1386
Elio Struyf <elio@struyfconsulting.be>
2022-12-17 19:11:52 +0100

    Update header

--- components/Page/Header.tsx
+++ components/Page/Header.tsx
@@ -61,38 +61,12 @@ export const Header: React.FunctionComponent<IHeaderProps> = forwardRef(({navIte
                 listClassName='flex gap-6'/>
 
               <SocialNavigation />
+
+              <Searchbox />
             </div>
           </div>
         </div>
       </header>
-
-      {/* <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-        <div className={`relative w-full py-6 flex items-center ${ isDocs ? "justify-center lg:justify-between" : "justify-between" }  border-b border-teal-500 xl:border-none`}>
-          <div className="flex items-center">
-
-            <div className="xl:hidden absolute left-0">
-              {
-                navItems && navItems.length > 0 && (
-                  <MobileMenu navItems={navItems} />
-                )
-              }
-            </div>
-
-            <Link href="/">
-              <a title={Extension.name}>
-                <span className="sr-only">{Extension.name}</span>
-                <Logo className={`text-whisper-500 hover:text-teal-500 h-16 w-auto`} />
-              </a>
-            </Link>
-
-            <div className="xl:hidden absolute right-0">
-              <Searchbox />
-            </div>
-          </div>
-          
-          <Navigation />
-        </div>
-      </nav> */}
     </>
   );
 });
