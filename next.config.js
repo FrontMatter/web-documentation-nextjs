df6b5fd53b34e04f2d9ca4ab059fc561a12dd7ee
Elio Struyf <elio@struyfconsulting.be>
2023-03-10 11:22:57 +0100

    Update cors

--- next.config.js
+++ next.config.js
@@ -18,7 +18,13 @@ module.exports = {
       headers: [{
         key: "Access-Control-Allow-Origin",
         value: "*"
-      }, ],
+      }, {
+        key: "Access-Control-Allow-Methods",
+        value: "GET,OPTIONS,POST"
+      }, {
+        key: "Access-Control-Allow-Headers",
+        value: "*"
+      }],
     }, ]
   },
 }
