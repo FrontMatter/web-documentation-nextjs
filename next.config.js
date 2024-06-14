09296b774916fbacffc393105833774ab703ac6f
Elio Struyf <elio@struyfconsulting.be>
2024-06-14 11:28:14 +0200

    Updates

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-if (process.env.NODE_ENV === "development") {
-  const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev")
-  setupDevPlatform()
-}
-
 module.exports = {
   reactStrictMode: false,
   env: {
